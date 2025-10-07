46bbc6ae4e88f58aa173d14105d6ffa0a7b58cf7
LucaCiani <lucaciani88@gmail.com>
2025-10-07 16:07:43 +0200

    bonus snack 2 complete

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,21 +1,35 @@
-function lanciaDado() {
-    const promessa = new Promise((resolve, reject) => {
-        console.log("Lancio il dado...");
+function creaLanciaDado() {
+    let ultimoRisultato = null;
 
-        setTimeout(() => {
-            let dado = Math.floor(Math.random() * 6 + 1);
-            let possibilita1 = Math.floor(Math.random() * 5 + 1);
-            let possibilita2 = Math.floor(Math.random() * 5 + 1);
+    return function lanciaDado() {
+        const promessa = new Promise((resolve, reject) => {
+            console.log("Lancio il dado...");
 
-            if (possibilita1 == possibilita2) {
-                reject("Il dado si è incastato");
-            } else {
-                resolve(`È uscito ${dado}`);
-            }
-        }, 3000);
-    });
-    return promessa;
+            setTimeout(() => {
+                let dado = Math.floor(Math.random() * 6 + 1);
+                let possibilita1 = Math.floor(Math.random() * 5 + 1);
+                let possibilita2 = Math.floor(Math.random() * 5 + 1);
+
+                if (possibilita1 == possibilita2) {
+                    reject("Il dado si è incastato");
+                } else {
+                    if (dado === ultimoRisultato) {
+                        console.log("Incredibile!");
+                    }
+                    ultimoRisultato = dado;
+                    resolve(`È uscito ${dado}`);
+                }
+            }, 3000);
+        });
+        return promessa;
+    };
 }
+
+const lanciaDado = creaLanciaDado();
+
+lanciaDado()
+    .then((messaggio) => console.log(messaggio))
+    .catch((err) => console.log(err));
 
 lanciaDado()
     .then((messaggio) => console.log(messaggio))
